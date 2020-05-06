fafd7dba23f4447c44892302e3db4047642c016f
FreshyMichael <michael@freshysites.com>
2020-05-06 17:59:54 -0400

    Update update-this-plugin-name.php

--- update-this-plugin-name.php
+++ update-this-plugin-name.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Plugin Name: Add PluginName
-* Plugin URI: https://freshysites.com/
+* Plugin URI: https://github.com/FreshyMichael/Plugin-Starter
 * Description: Add a Description
 * Version: 1.0.0
 * Author: FreshySites
@@ -14,6 +14,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /* PluginName Start */
+//______________________________________________________________________________
 
+
+//______________________________________________________________________________
+// All About Updates
+
+//  Begin Version Control | Auto Update Checker
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+// ***IMPORTANT*** Update this path to New Github Repository Master Branch Path
+	'https://github.com/FreshyMichael/Plugin-Starter',
+	__FILE__,
+// ***IMPORTANT*** Update this to New Repository Master Branch Path
+	'Plugin-Starter'
+);
+//Enable Releases
+$myUpdateChecker->getVcsApi()->enableReleaseAssets();
+//Optional: If you're using a private repository, specify the access token like this:
+//
+//
+//Future Update Note: Comment in these sections and add token and branch information once private git established
+//
+//
+//$myUpdateChecker->setAuthentication('your-token-here');
+//Optional: Set the branch that contains the stable release.
+//$myUpdateChecker->setBranch('stable-branch-name');
+
+//______________________________________________________________________________
 /* PluginName End */
 ?>
