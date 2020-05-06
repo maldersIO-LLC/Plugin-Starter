7a94837982dab8c2eb2ffa6c2cddce316b4e57eb
FreshyMichael <michael@freshysites.com>
2020-05-06 17:17:54 -0400

    Create update-this-plugin-name.php

---
+++ update-this-plugin-name.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Plugin Name: Add PluginName
+* Plugin URI: https://freshysites.com/
+* Description: Add a Description
+* Version: 1.0.0
+* Author: FreshySites
+* Author URI: https://freshysites.com/
+* License: GNU v3.0
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+/* PluginName Start */
+
+/* PluginName End */
+?>
